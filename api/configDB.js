d152cdc5263f6d4e820e2eab451be0a0aeda07c8
Phạm Minh Dũng <64119844+PMD2102@users.noreply.github.com>
2020-11-07 05:57:13 +0700

    Update configDB.js

--- api/configDB.js
+++ api/configDB.js
@@ -4,8 +4,8 @@ module.exports = {
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: 'AKIAIQPSX73QITKJQCNA',
-      secretAccessKey: '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',
+      accessKeyId: '',
+      secretAccessKey: '',
       region: 'us-east-2',
     }
 };
